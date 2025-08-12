11ea8e781a00e4ee8960d82dd3c93098ebbc57df
marlonknupp <marlonjcc23@gmail.com>
2025-08-12 14:37:10 -0300

    ...

--- core/settings.py
+++ core/settings.py
@@ -82,9 +82,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # caminho do arquivo SQLite
     }
 }
+
 
 
 # Password validation
